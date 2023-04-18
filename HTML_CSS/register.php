f7f5c1c54de4e6bf70f727f4a2e6979f7d348f5a
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2023-04-18 19:39:13 +0530

    Some minor PHP changes in WPL

---
+++ HTML_CSS/register.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+$username = $_POST['username'];
+// $email = $_POST['email'];
+// $pass = $_POST['pass'];
+echo $_POST['name'].$name;
+// include("database.php");
+// $sql=mysqli_query($conn,"SELECT * FROM userdata where Email='$email'");
+// if(mysqli_num_rows($sql)>0)
+// {
+//     echo "Email Id Already Exists"; 
+// 	exit;
+// }
+// $sql=mysqli_query($conn,"SELECT * FROM userdata where Username='$username'");
+// if(mysqli_num_rows($sql)>0)
+// {
+//     echo "Username taken!"; 
+// 	exit;
+// }
+?>
