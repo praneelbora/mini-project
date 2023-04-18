8c4230c603dd518f0e2f30ae59e108c19aa0fbcc
Praneel Bora <praneelbora@gmail.com>
2023-04-18 15:16:17 +0530

    bootstrap navbar

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -3,11 +3,13 @@ import { SessionProvider } from 'next-auth/react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Head from 'next/head';
 export default function App ({ Component, pageProps: { session, ...pageProps } }){
+
     return (
     <SessionProvider session={session}>
         <Head>
             <meta name='viewport' content='width=device-width, initial-scale=1'/>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css" />
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         </Head>
         
     <Component {...pageProps} />
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -7,49 +7,90 @@ import el2 from '../public/Ellipse 2.png'
 import el3 from '../public/Ellipse 3.png'
 import styles from '../styles/styles.module.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useState, useEffect } from 'react';
 import { useSession, signOut, signIn, signUp } from 'next-auth/react';
 
 export default function Home(){
+  const [clientWindowHeight, setClientWindowHeight] = useState("");
+  const [backgroundTransparency, setBackgroundTransparency] = useState(0);
+  const [linkColor, setLinkColor] = useState('black');
 
-  const popupCenter = (url, title) => {
-    const dualScreenLeft = window.screenLeft ?? window.screenX;
-    const dualScreenTop = window.screenTop ?? window.screenY;
-    const width =
-      window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
+    const handleScroll = () => {
+      setClientWindowHeight(window.scrollY);
+    };
 
-    const height =
-      window.innerHeight ??
-      document.documentElement.clientHeight ??
-      screen.height;
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll); 
+      return () => window.removeEventListener("scroll", handleScroll);
+      });
+      useEffect(() => {
+        if (clientWindowHeight>10){
+            setBackgroundTransparency(100);
+            setLinkColor('white');
+        }
+      }, [clientWindowHeight]);
+    const { data: session } = useSession();
 
-    const systemZoom = width / window.screen.availWidth;
 
-    const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
-    const top = (height - 550) / 2 / systemZoom + dualScreenTop;
-
-    const newWindow = window.open(
-      url,
-      title,
-      `width=${500 / systemZoom},height=${550 / systemZoom
-      },top=${top},left=${left}`
-    );
-
-    newWindow?.focus();
-  };
-
-  const { data: session } = useSession();
     return (
       <div className={styles.upperbody}>
         {/* NAVBAR  */}
-        <nav className={styles.navbar} id="navbar">
+        {/* <nav className={styles.navbar} id="navbar">
           <Image className={styles.logo} src={logo} alt="Logo" />
           <ul className={styles.ull} style={{listStyleType: 'none'}}>
             <li className={`${styles.nav1} ${styles.nav2}`} style={{color: 'white'}}><a href="#browse" style={{color: 'inherit', fontStyle: 'normal', textDecoration: 'none'}}>Browse</a></li>
             <li className={`${styles.nav1} ${styles.nav2}`} style={{color: 'white'}}>About Us</li>
             <li className={`${styles.nav1} ${styles.nav2}`} style={{color: 'white'}}>Contact</li>
-            {!session?<li className={styles.nav1}><span><Link href="/newlogin" target='_blank' className={styles.nav3}>Log In</Link></span> | <span className={styles.nav3}><Link href="/newsignup" target='_blank' className={styles.nav3}>Sign Up</Link></span> </li>:<li className={styles.nav1}><span>Hi</span> <Link href={'/myacc'} className={styles.user}>{session.user.username}</Link> | <Link href={'/'} onClick={() => signOut()} className={styles.nav3}>Sign Out</Link></li>}
+            {!session?<li className={styles.nav1}><span><Link href="/newlogin" target='_blank' className={styles.nav3}>Log In</Link></span> | <span className={styles.nav3}><Link href="/newsignup" target='_blank' className={styles.nav3}>Sign Up</Link></span> </li>:<li className={styles.nav1}><span>Hi</span> <Link href={'/myacc'} className={styles.nav2}>{session.user.username}</Link> | <Link href={'/'} onClick={() => signOut()} className={styles.nav3}>Sign Out</Link></li>}
           </ul>
+        </nav> */}
+        <nav className= {`navbar navbar-expand-lg ${styles.navbar}`} style={{background: `rgb(53, 162, 78, ${backgroundTransparency})`}}>
+          <div className="container-fluid"style={{backfaceVisibility:'revert'}}>
+            <Link className="navbar-brand ms-3" href="/"><Image className={styles.logo} src={logo} alt='Logo'/></Link>
+            
+            {/* <div className='navbar-brand'></div> */}
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className={`${styles.ull} navbar-nav ms-auto mb-2 mb-lg-0 me-3`}>
+                <li className="nav-item mx-2">
+                  <Link className="nav-link active" aria-current="page" href="#">Browse</Link>
+                </li>
+                <li className="nav-item mx-2 ">
+                  <Link className="nav-link active" aria-current="page" href="#">About Us</Link>
+                </li>
+                <li className="nav-item mx-2">
+                  <Link className="nav-link active" aria-current="page" href="#">Contact</Link>
+                </li>
+                <li className="nav-item mx-2">
+                  <Link className="nav-link active" aria-current="page" href="/newlogin" target='_blank'>Log In</Link>                  
+                </li> 
+                  {/* <Link className='nav-link'>|</Link> */}
+                <li className="nav-item mx-2">
+                  <Link className="nav-link active" aria-current="page" href="/newsignup"target='_blank'>Sign Up</Link>                  
+                </li>
+                
+
+                {/* <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><hr className="dropdown-divider"/></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li> */}
+                
+              </ul>
+              
+            </div>
+          </div>
         </nav>
+
+
         {/* HERO SECTION  */}
         <section className={styles.hero}>
           <div className={styles.lorem}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolore adipisci ullam sequi aperiam perferendis quas, quaerat aspernatur architecto, molestias numquam cum perspiciatis esse excepturi vitae aliquid odio doloribus nihil.</div>
@@ -61,7 +102,7 @@ export default function Home(){
         <section className={styles.search} id="browse">
           <input className={styles.searchbar} type="text" placeholder="Hotels, Places, Landmarks etc" />
         </section>
-        {/* <section class="contact">
+        {/* <section className="contact">
         <h1 style="margin-left: 0;">Contact Us</h1>
         <p style="padding-top: 1%;">abc@gmail.com</p>
         <p style="padding-top: 1%;">1234567890</p>
