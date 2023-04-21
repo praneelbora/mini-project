6ae73fd8bd5138030294bb1d7d038da5fb11c8c5
Praneel Bora <praneelbora@gmail.com>
2023-04-21 11:41:30 +0530

    moved html css

--- HTML_CSS/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $url='localhost';
-    $rootusername='root';
-    $password='';
-    $conn=mysqli_connect($url,$rootusername,$password,"form_data");
-    if(!$conn){
-        die('Could not Connect My Sql:' .mysql_error());
-    }
-?>
