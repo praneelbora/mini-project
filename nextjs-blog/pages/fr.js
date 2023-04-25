7e67974cac5f63eef5b79cee69052627374df0a7
Praneel Bora <praneelbora@gmail.com>
2023-04-25 15:12:49 +0530

    working on full review page which opens on new page

--- nextjs-blog/pages/fr.js
+++ nextjs-blog/pages/fr.js
@@ -4,8 +4,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import LinesEllipsis from 'react-lines-ellipsis';
 import Im from '../public/Bg_1.png';
 import css from '../styles/fr.module.css'
+import { useState } from 'react';
 export default function VReview({ dashImg, title, reviewer, desc, rating, eagleScore, country, city, upvotes, upvoted, createdAt })
 {   
+  const [closeState,setCloseState] = useState('bi-x-square text-success');
   const newdate = createdAt
   const date = new Date(createdAt).toLocaleString("en-GB", {
     day: "numeric",
@@ -14,21 +16,40 @@ export default function VReview({ dashImg, title, reviewer, desc, rating, eagleS
     hour: "numeric",
     minute: "2-digit"
   });
+
+    async function onClose()
+    {
+      window.close();
+    }
   
     return(
-      <div className={css.box}>
-      
-      <h1>Title Of Review</h1>
-      {/* <p className={css.deets}>@ username</p> */}
-      <div className={`${css.deets} mt-3`}>
-        {/* <br/> */}
-        <Image src={Im} alt="Img" className={css.i}></Image>
-        <p className="mt-2" > <span style={{float: 'left',marginLeft:'1vw'}}>@ username</span><span style={{float: 'right',marginRight:'1vw'}}>Time</span> </p>
-        <br/>
-
-
-      </div>
-      </div>
+      <>
+        <div style={{position:'fixed', right:'2vw', top:'2vh' }} onClick={()=>onClose()} onMouseEnter={()=>setCloseState('bi-x-square-fill text-success')} onMouseLeave={()=>setCloseState('bi-x-square')}>
+        <i class={`bi ${closeState}`} style={{fontSize:'50px'}}></i>
+              </div>
+              <div className={css.box}>
+       
+       <h1>{title}</h1>
+       {/* <p className={css.deets}>@ username</p> */}
+       <div className={`${css.deets} mt-3`}>
+         {/* <br/> */}
+         <Image src={Im} alt="Img" className={css.i} width={500} height={500}></Image>
+         {/* <Image src={dashImg} alt="Img" className={css.i}></Image> */}
+         <p className="mt-2" > <span style={{float: 'left',marginLeft:'2vw'}}><b>Username: </b>@ username</span><span style={{float: 'right',marginRight:'2vw'}}><b>Date: </b>25-04-2023</span> </p>
+         {/* <p className="mt-2" > <span style={{float: 'left',marginLeft:'2vw'}}><b>Username: </b>{reviewer}</span><span style={{float: 'right',marginRight:'2vw'}}><b>Date: </b>{date}</span> </p> */}
+         <br/>
+         <p className="mt-2" > <span style={{float: 'left',marginLeft:'2vw'}}><b>Rating: </b>4.5</span><span style={{float: 'right',marginRight:'2vw'}}><b>EagleScore: </b> 8</span> </p>
+         {/* <p className="mt-2" > <span style={{float: 'left',marginLeft:'2vw'}}><b>Rating: </b>{rating}</span><span style={{float: 'right',marginRight:'2vw'}}><b>EagleScore: </b> {eagleScore}</span> </p> */}
+         <br/>
+         {/* <p className='mt-2' style={{float: 'left',marginLeft:'2vw'}}><b>Place: </b>{Hotel}, {city}, {country}</p> */}
+         <p className='mt-2' style={{float: 'left',marginLeft:'2vw'}}><b>Place: </b>{city}, {country}</p>
+         <br/>
+         <br/>
+         <p>{desc}</p>
+       </div>
+       </div>
+       
+      </>
       
 
 
