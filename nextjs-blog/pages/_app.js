e9c1eebea062fd15a28d6fc9c817d52ffdf89c8c
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2023-05-02 11:59:35 +0530

    Tab details changed

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -8,6 +8,7 @@ export default function App ({ Component, pageProps: { session, ...pageProps } }
     <SessionProvider session={session}>
         <Head>
             <meta name='viewport' content='width=device-width, initial-scale=1'/>
+            <title>EagledEye</title>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css" />
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossOrigin="anonymous"></script>
         </Head>
